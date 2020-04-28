fdd2a7bea73b8ec0dbfc3b984aece4e621df8325
André Luferat <andre@luferat.net>
2020-04-28 13:38:40 -0300

    Formulário com CSS e responsivo

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-form',
@@ -19,6 +20,48 @@ export class FormComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+    $(document).ready(() => { // JQuery
+      $(window).resize(() => {
+        if (window.innerWidth > 539) {
+          $('aside').show(0);
+        } else {
+          $('aside').hide(0);
+        }
+      });
+    });
+  }
+
+  // Oculta / exibe ajuda
+  helpToggle() {
+    $(document).ready(() => {
+
+      if ($('aside').is(':visible')) {
+        this.helpHide();
+      } else {
+        this.helpShow();
+      }
+
+    });
+    return false;
+  }
+
+  // Oculta ajuda
+  helpHide() {
+    $('aside').slideUp('fast');
+  }
+
+  // Mostra ajuda
+  helpShow() {
+    $('aside').slideDown('fast');
+  }
+
+  hideAside() {
+    if (window.innerWidth > 539) {
+      return false;
+    } else {
+      this.helpHide();
+    }
   }
 
 }
