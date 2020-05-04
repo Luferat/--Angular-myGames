66accf3dc7d84380879cfe55d1b0559c28b6e221
André Luferat <contatos@luferat.net>
2020-05-04 08:57:15 -0300

    Ajustes menores

---
+++ src/app/classes/game-form.ts
@@ -0,0 +1,9 @@
+export class GameForm {
+  id: string;
+  cover: string;
+  date: string;
+  description: string;
+  media: string;
+  platform = '';
+  title: string;
+}
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -3,12 +3,18 @@ import { Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
 import * as $ from 'jquery';
 
+// Importa a classe dos campos do formulário
+import { GameForm } from '../classes/game-form';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+
+  // Declara campos do formulário
+  gameForm: GameForm = new GameForm();
 
   // Armazena os nomes das plataformas
   platforms: Observable<any[]>;
@@ -31,6 +37,43 @@ export class FormComponent implements OnInit {
       });
     });
   }
+
+  // Método que proicessa o formulário
+  onSubmit() {
+    console.log(this.gameForm);
+
+    // Se não identificou um id cadastre um novo jogo
+    if (this.gameForm.id === undefined) {
+
+      // Cadastrar novo jogo ({...} é um atalho para JSON Parse)
+      this.db.collection<any>('games').add({ ...this.gameForm })
+        .then(() => {
+
+          // Feedback postivo para o usuário
+          alert(`Jogo "${this.gameForm.title}" adicionado com sucesso!\n\nClique em [Ok] para continuar.`);
+
+          // Apaga os valores dos campos do formulário
+          this.gameForm = new GameForm();
+
+          // Sai sem fazer mais nada
+          return false;
+        })
+        .catch((err) => {
+          // Exibe erros no console
+          console.error('Erro ao gravar dados: ' + err);
+        });
+
+    } else {
+
+      // Edita o jogo indicado pelo Id
+
+    }
+
+
+  }
+
+
+
 
   // Oculta / exibe ajuda
   helpToggle() {
@@ -56,6 +99,7 @@ export class FormComponent implements OnInit {
     $('aside').slideDown('fast');
   }
 
+  // Oculta ajuda pare resoluções baixas
   hideAside() {
     if (window.innerWidth > 539) {
       return false;
